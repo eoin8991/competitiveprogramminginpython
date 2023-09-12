fb760211c1cde9157deb4759e47b854abe0d2d8d
eoin8991 <142859309+eoin8991@users.noreply.github.com>
2023-09-12 14:49:11 -0400

    Update minimalcoverage.py

--- minimalcoverage.py
+++ minimalcoverage.py
@@ -40,3 +40,90 @@ output
 1
 0 1
 '''
+
+import sys
+
+inf = 200000
+
+class Pair:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+cover = [inf] * 6000
+p = [0] * 6000
+seg = [Pair(0, 0)] * 100000
+N = 0
+M = 0
+
+def min(a, b):
+    return a if a < b else b
+
+def max(a, b):
+    return a if a > b else b
+
+def compare(a, b):
+    p1 = a
+    p2 = b
+    return p1.first - p2.first
+
+def init():
+    global N, M
+    for i in range(N):
+        cover[i] = inf
+    seg.sort(key=lambda x: x.first)
+    for i in range(M):
+        for j in range(min(N-1, seg[i].second), seg[i].first, -1):
+            if cover[j] != inf:
+                break
+            cover[j] = seg[i].first
+            p[j] = i
+
+sol = [Pair(0, 0)] * 100000
+S = 0
+
+def get_covering():
+    global N, sol, S
+    i = N - 1
+    covering = 0
+    while i > 0:
+        if cover[i] == inf:
+            return inf
+        covering += 1
+        sol[S] = seg[p[i]]
+        S += 1
+        i = cover[i]
+    return covering
+
+N = int(input())
+N += 1
+M = 0
+seg[M].first, seg[M].second = map(int, input().split())
+while seg[M].first or seg[M].second:
+    M += 1
+    seg[M].first, seg[M].second = map(int, input().split())
+    if seg[M].first > seg[M].second:
+        temp = seg[M].first
+        seg[M].first = seg[M].second
+        seg[M].second = temp
+
+init()
+covering = get_covering()
+if covering == inf:
+    print("No solution")
+    sys.exit(0)
+print(covering)
+for i in range(S-1, -1, -1):
+    print(sol[i].first, sol[i].second)
+sys.exit(0)
+
+'''
+code solution inspiration from marioyc
+'''
+
+'''
+Key Concepts:
+-defining a class by initializing attributes through __init__()
+-sorting using the lambda function
+-exiting a system using sys.exit(0)
+'''
