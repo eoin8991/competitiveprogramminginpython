2f0cc4ab4393e66af8266b516b33ab5e4968dc74
eoin8991 <142859309+eoin8991@users.noreply.github.com>
2023-09-12 15:00:23 -0400

    Update FIBOSUM.py

--- FIBOSUM.py
+++ FIBOSUM.py
@@ -34,3 +34,52 @@ Constraints
 T <= 1000
 0 <= N <= M <= 109
 '''
+
+MOD = 1000000007
+
+def multiply(F, M):
+    x = (F[0][0] * M[0][0] + F[0][1] * M[1][0]) % MOD
+    y = (F[0][0] * M[0][1] + F[0][1] * M[1][1]) % MOD
+    z = (F[1][0] * M[0][0] + F[1][1] * M[1][0]) % MOD
+    w = (F[1][0] * M[0][1] + F[1][1] * M[1][1]) % MOD
+    F[0][0] = x
+    F[0][1] = y
+    F[1][0] = z
+    F[1][1] = w
+
+def power(F, n):
+    if n == 0 or n == 1:
+        return
+    M = [[1, 1], [1, 0]]
+    power(F, n // 2)
+    multiply(F, F)
+    if n % 2 != 0:
+        multiply(F, M)
+
+def fib(n):
+    F = [[1, 1], [1, 0]]
+    if n == 0:
+        return 0
+    power(F, n - 1)
+    return F[0][0]
+
+t = int(input())
+while t > 0:
+    n, m = map(int, input().split())
+    if n > m:
+        print("Error!!")
+    else:
+        result = (fib(m + 2) - fib(n + 1)) % MOD
+        if result < 0:
+            result += MOD
+        print(result)
+    t -= 1
+
+'''
+code development inspiration from kashsingh
+'''
+
+'''
+Key Concepts:
+-modulo vs integer division
+'''
