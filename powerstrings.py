2970521cf4819426e99fbeaca7a789c767dbdfed
eoin8991 <142859309+eoin8991@users.noreply.github.com>
2023-09-13 12:30:38 -0400

    Update powerstrings.py

--- powerstrings.py
+++ powerstrings.py
@@ -50,3 +50,63 @@ Output
 3
 
 '''
+
+#Two solutions
+#1 
+
+import sys
+
+def check(s, n):
+    for i in range(n, len(s)):
+        if s[i] != s[i-n]:
+            return False
+    return True
+
+def main():
+    s = input().strip()
+    while s != ".":
+        n = len(s)
+        for i in range(1, n+1):
+            if n % i == 0:
+                if check(s, i):
+                    print(n//i)
+                    break
+        s = input().strip()
+
+if __name__ == "__main__":
+    main()
+
+'''
+code development inspiration from mpfeifer1
+'''
+
+#2
+
+from sys import stdin
+
+def find_divisors(n):
+	divisors = []
+	maxi = int((n**(0.5)+1))
+	for i in range(1, maxi+1):
+		if n % i == 0:
+			divisors.append(i)
+			divisors.append(n//i)
+	return sorted(list(set(divisors)))
+
+line = stdin.readline().strip()
+while line != ".":
+	length = len(line)
+	divisors = find_divisors(length)
+	for n in divisors:
+		if line[-n:]*(length//n) == line:
+			ans = (length//n)
+			break
+	print(ans)
+	line = stdin.readline().strip()
+
+'''
+code development credit from xCiaraG
+'''
+
+###the code does not seem to be printing the desired n, but it's possible
+#this kattis question has evolved over time
